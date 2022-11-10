03f40f65bb808daa3e1741bf2dea00a16cf7809c
berhanusg@gmail.com <berhanusg@gmail.com>
2022-11-09 22:03:34 -0800

    Controller for Candidate and service partially done

--- src/main/java/com/pyramidbuildersemployment/controller/CandidateController.java
+++ src/main/java/com/pyramidbuildersemployment/controller/CandidateController.java
@@ -1,4 +1,35 @@
 package  com.pyramidbuildersemployment.controller;
+
+import com.pyramidbuildersemployment.models.Address;
+import com.pyramidbuildersemployment.models.Candidate;
+import com.pyramidbuildersemployment.service.CandidateService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/Candidate")
+
+
 public class CandidateController {
+
+    private CandidateService candidateService;
+
+    public CandidateController(CandidateService candidateService) {
+        this.candidateService = candidateService;
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Candidate> getCandidateById(@PathVariable long id) {
+        Candidate candidate = candidateService.getCandidateById(id);
+
+        if (candidate != null) {
+            // send a 200 status code with the user object as the response body
+            return ResponseEntity.ok(candidate);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+
+    }
     
 }
--- src/main/java/com/pyramidbuildersemployment/service/CandidateService.java
+++ src/main/java/com/pyramidbuildersemployment/service/CandidateService.java
@@ -1,5 +1,16 @@
 package com.pyramidbuildersemployment.service;
 
+import com.pyramidbuildersemployment.models.Candidate;
+
 public class CandidateService {
-    
+
+    public Candidate getCandidateBy(long id) {
+
+        return null;
+    }
+
+    public Candidate getCandidateById(long id) {
+
+        return null;
+    }
 }
